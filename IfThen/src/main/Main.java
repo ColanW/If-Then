217c3b1d1108b8fb2fb69690c3dacedc918e4b60
ColanW <54151642+ColanW@users.noreply.github.com>
2019-11-30 04:09:34 -0600

    Add files via upload

---
+++ IfThen/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+		boolean flip = true;
+		
+		if(flip==true) {
+			System.out.println("true");
+		}else if(flip==false){
+			System.out.println("false");
+		}
+		
+	}
+	
+}
